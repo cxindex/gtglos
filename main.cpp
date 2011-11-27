bc7e3e81e81541f33fc9763c703ef9c5b134d0ae
cx uname <cx.index@gmail.com>
2011-11-27 18:50:18 +0400

    -some comments

--- main.cpp
+++ main.cpp
@@ -20,18 +20,6 @@ base dynbox;
 base dyn_array[256];
 base stat_array[256];
 
-
-//~ base *shots_array = (base*)malloc(sizeof(base*5));
-//~ base *shots_array = new base[5];
-//~ base *shots_array;
-
-//~ void add_to_dyn_array(base add)
-//~ {
-	//~ static int to;
-	//~ dyn_array[to++]=add;
-//~ }
-
-
 struct distance_hit{
 	base b;
 	int state;
@@ -45,43 +33,21 @@ static void nearCallback (void *data, dGeomID o1, dGeomID o2)
 	
 	char *gd = 0;
 	gd = (char*) dGeomGetData (o1);
-	//~ gd = (char*) dGeomGetClassData (o2);
-	//~ fprintf(stderr, "%s\n", gd);
-	//~ gd = (char*) dGeomGetData (o2);
-	//~ fprintf(stderr, "%s\n", gd);
-
-	// exit without doing anything if the two bodies are connected by a joint
-	//~ if (b1 && b2 && dAreConnectedExcluding (b1,b2,dJointTypeContact)) return;
 	
 	dContact contact[MAX_CONTACTS];   // up to MAX_CONTACTS contacts per box-box
 	for (i=0; i<MAX_CONTACTS; i++)
 	{
-//		contact[i].surface.mode = dContactSoftCFM ;
 		contact[i].surface.mode = dContactSoftCFM |  dContactSoftERP;
-		//~ contact[i].surface.mu = 17;
-		//~ contact[i].surface.mu = 1;
-//		contact[i].surface.slip1 = 10.1;
-//		contact[i].surface.slip2 = 1000.1;
-
-//		contact[i].surface.mu = dInfinity;
-//		contact[i].surface.mu = 7;
 		contact[i].surface.mu = 5;
-//		contact[i].surface.bounce = 0.0;
-//		contact[i].surface.bounce_vel = 0.0;
 		contact[i].surface.soft_cfm = 0.03;
-//		contact[i].surface.soft_cfm = 0.003;
 		contact[i].surface.soft_erp = 0.1;
 	}
 	
 	if(int numc = dCollide (o1, o2, MAX_CONTACTS, &contact[0].geom, sizeof(dContact)))
 	{
-//		printf("D: %s\n", dGeomGetData(o1));
 		dGeomSetData (o1, (void*)'1');
 		dGeomSetData (o2, (void*)'1');
 
-	    //~ dMatrix3 RI;
-	    //~ dRSetIdentity (RI);
-	    //~ const dReal ss[3] = {0.02,0.02,0.02};
 	    for (i=0; i<numc; i++)
 	    {
 			if (dGeomGetClass(o1) == dRayClass || dGeomGetClass(o2) == dRayClass)
@@ -125,40 +91,6 @@ void look_at(dBodyID body)
 	static int w_func=old[0]=w_window;
 	static int h_func=old[1]=h_window;
 	
-// 	if(old[0]!=w_window){
-// 		// if(w_func>w_window) w_func=-w_window;
-// 		if(w_func<w_window){
-// 			w_func=-w_window-old[0]; //w_func=+w_window;
-// //			x_shear=-w_window-w_func;
-// 		}
-// 		if(w_func>w_window){
-// 			w_func=-old[0]-w_window; //w_func=+w_window;
-// 			//	x_shear=-w_window-w_func;
-// 		}
-		
-// 		printf("hello W");
-// 		old[0]=w_window;
-// //		old[1]=h_window;
-
-// 	}
-	
-// 	if(old[1]!=h_window){
-// 		// if(w_func>w_window) w_func=-w_window;
-// 		if(h_func<h_window){
-// 			h_func=-h_window-old[1]; //w_func=+w_window;
-// //			x_shear=-w_window-w_func;
-// 		}
-// 		if(h_func>h_window){
-// 			h_func=-old[0]-h_window; //w_func=+w_window;
-// 			//	x_shear=-w_window-w_func;
-// 		}
-		
-// 		printf("hello H");
-// 		old[1]=h_window;
-// //		old[1]=h_window;
-
-// 	}
-	///////
 	//x
 	if((odepos[0]+250)>w_func)
 	{
@@ -188,10 +120,9 @@ void look_at(dBodyID body)
 	//y
 	if((odepos[1]+50)>h_func)
 	{
-		// scroll=fabsf(pointrelvel[1]/10);
-		// if (scroll == 0) scroll = 2;
-		scroll=0;
-
+		scroll=fabsf(pointrelvel[1]/10);
+		if (scroll == 0) scroll = 2;
+		
 		 if((odepos[1])>h_func)
 		 	scroll=h_window-60;
 
@@ -201,12 +132,11 @@ void look_at(dBodyID body)
 	}	
 	else if((odepos[1]-50)<h_func-h_window)
 	{
-		// scroll=fabsf(pointrelvel[1]/10);
-		// if (scroll == 0) scroll = 2;
-		scroll=0;
+		scroll=fabsf(pointrelvel[1]/10);
+		if (scroll == 0) scroll = 2;
+
 		 if((odepos[1])<h_func-h_window)
 		 	scroll=h_window-60; //bottom edge
-//		 	scroll=y_shear_g+60; //bottom edge
 
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
@@ -300,35 +230,6 @@ void render_scene(void)
 	t1.active_square_render();
 	//приоритетные ниже
 	
-	//---------------------------------
-	//~ glMatrixMode(GL_MODELVIEW);
-	//~ glLoadIdentity();
-	//~ glColor4f(0.0, 256.0, 0.0, 256.0);
-	//~ 
-	//~ glRasterPos2f(w_func_g-w_window+1, h_func_g-10);
-	//~ char ode[100];
-	//~ sprintf(ode, "T1:ODE// X: %.3f Y: %.3f Z:%.3f", t1.odepos[0], t1.odepos[1], t1.odepos[2]);
-	//~ print_bitmap_string(GLUT_BITMAP_8_BY_13, ode);
-	//~ 
-	//~ glRasterPos2f(w_func_g-w_window+1, h_func_g-25);
-	//~ char gl[100];
-	//~ sprintf(gl, "T1:GL// X: %.3f Y: %.3f", t1.x, t1.y);
-	//~ print_bitmap_string(GLUT_BITMAP_8_BY_13, gl);
-//~ 
-	//~ glRasterPos2f(w_func_g-w_window+1, h_func_g-40);
-	//~ char dbode[100];
-	//~ sprintf(dbode, "DB:ODE// X: %.3f Y: %.3f Z:%.3f", dynbox.odepos[0], dynbox.odepos[1], dynbox.odepos[2]);
-	//~ print_bitmap_string(GLUT_BITMAP_8_BY_13, dbode);
-	//~ 
-	//~ if ( (currentTime - lastUpdate) >= 1000 )
-	//~ {
-		//~ sprintf( fpsS, "GFPS: %i", frames );
-		//~ frames = 0;
-		//~ lastUpdate = currentTime;
-	//~ }
-	//~ glRasterPos2f(w_func_g-w_window+1, h_func_g-55);
-	//~ print_bitmap_string(GLUT_BITMAP_8_BY_13, fpsS);
-	//---------------------------------
 	static int i2=0;
 	i2=0;
 	while(i2!=5)
@@ -350,17 +251,11 @@ void keyboard_func(unsigned char key, int x, int y)
 {
 	if (keystates[(int)key] == 1) keystates[(int)key] = 2;
 	if (keystates[(int)key] == 0) keystates[(int)key] = 1;
-	//~ fprintf(stderr, "%i DOWN\t", (int)key);
-	//~ fprintf(stderr, "%i DOWN\n", keystates[(int)key]);
-
 }
 
 void keyboard_up_func(unsigned char key, int x, int y)
 {
 	keystates[(int)key]=0;
-	//~ fprintf(stderr, "%i UP\t\n", (int)key);
-
-	//~ fprintf(stderr, "%i %i\n", x, y);
 }
 
 void mouse_action(int button, int state, int x, int y)
@@ -376,7 +271,6 @@ void mouse_action(int button, int state, int x, int y)
 	  dyn_array[to].body = dBodyCreate (world);
 	  dJointAttach (plane2d, dyn_array[to].body, 0);
 	  dyn_array[to].geom = dCreateBox (space, 60, 45, 10);
-//	  dMassSetBoxTotal (&dyn_array[to].m,0.9,60,45,10); //work
 	  dMassSetBoxTotal (&dyn_array[to].m,0.6,60,45,10); //work
 	  dyn_array[to].rotatebit=1;
 	  dyn_array[to].ode_init();
@@ -384,7 +278,6 @@ void mouse_action(int button, int state, int x, int y)
 	  dyn_array[to].img_load(60, 45, "block.bmp", 1, 0, 2);
 	  //why h_f_g? its top edge
 	  dyn_array[to].set_pos(x_shear_g+x, h_func_g-y);
-//	  fprintf(stderr, "s: %i\ny: %i\nh_f: %i\n\n", y_shear_g, y, h_func_g);
 	  to++;
 	}
 }
@@ -406,15 +299,10 @@ void timer_func(int value)
 	glutPostRedisplay();
 	simLoop(1);
 	glutTimerFunc(15,timer_func,1);
-	//мб вынести симлуп во вторую тайм_фунц
-
 }
 
 void reshape_window(GLsizei w, GLsizei h)
 {
-//	printf("reshape it!\n");
-//	w_window=w;
-//	h_window=h;
 }
 //--------------------------------- GL END
 
@@ -446,7 +334,6 @@ int main (int argc, char **argv)
 	t1.body = dBodyCreate (world);
 	dJointAttach( plane2d, t1.body, 0 );
 	    t1.geom = dCreateSphere (space,18); // / на большую сторону. дописать
-	    //~ dMassSetSphere (&t1.m, 1, 0.5); //coz active_control
 	    dMassSetSphereTotal (&t1.m, 0.4, 18);
 	    t1.ode_init();
 		
@@ -460,15 +347,12 @@ int main (int argc, char **argv)
 		t1.img_load(44, 34, "sprite/gasmask/ur1.bmp", 1, 7, 1);
 		t1.set_pos(0,100);
 		t1.speed=100;
-		dGeomSetData (t1.geom, (void*)'1');
-
 		//---------------------------------
 		
 		dynbox.body = dBodyCreate (world);
 		dJointAttach ( plane2d, dynbox.body, 0 );
 	    dynbox.geom = dCreateBox (space, 60, 45, 10);
 	    dMassSetBoxTotal (&dynbox.m,0.9, 60, 45, 10); //work
-	    //~ dMassSetBox (&dynbox.m,1, 1,1,1); //work
 	    dynbox.rotatebit=1;
    	    dynbox.ode_init();
    	    
@@ -491,10 +375,6 @@ int main (int argc, char **argv)
 		t2.set_pos(200,900);
 		t2.speed=100;
 		//---------------------------------
-		//~ base right_wall;
-		//~ right_wall.geom = dCreateBox (space, 400, 300, 10);
-		//~ right_wall.img_load(400, 300, "panel.bmp", 6, 0, 0); //with this == tex. withou == no_tex
-		//~ dGeomSetPosition (right_wall.geom, 400, 90, 0);
 		make_world_conf("1.conf", stat_array);
 //---------------------------------
 
